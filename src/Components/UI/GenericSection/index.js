88a9cac267172b0fac4daad880efd5f18443db83
DKSecurity99 <dksecurity@gmail.com>
2020-12-12 11:10:54 -0300

    refactor: moved component to UI folder

---
+++ src/Components/UI/GenericSection/index.js
@@ -0,0 +1,28 @@
+import Container from '../../../styles/container';
+
+import {
+  GenericSectionWrapper,
+  GenericSectionContainer,
+  GenericSectionContent,
+  GenericSectionTitle,
+  GenericSectionText,
+  GenericSectionChildrens,
+} from './styles';
+
+function GenericSection({ title, text, isBackgroundWhite, children}) {
+  return (
+    <GenericSectionWrapper isBackgroundWhite={isBackgroundWhite}>
+      <Container>
+        <GenericSectionContainer>
+          <GenericSectionContent>
+            <GenericSectionTitle>{ title }</GenericSectionTitle>
+            <GenericSectionText>{ text }</GenericSectionText>
+          </GenericSectionContent>
+          <GenericSectionChildrens>{ children }</GenericSectionChildrens>
+        </GenericSectionContainer>
+      </Container>
+    </GenericSectionWrapper>
+  );
+}
+
+export default GenericSection;
